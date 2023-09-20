7706213d10a8962b0df8c412909be0a83ba4afec
Joao <joaovitor2004568@gmail.com>
2023-09-19 21:37:52 -0300

    exemplo de array bidimensionais

---
+++ Arrays/bidimensional.js
@@ -0,0 +1,5 @@
+const alunos = ['João','Juliana','Ana','Caio'];
+const notas = [10,8,7.5,9];
+
+const alunosnotas = [alunos,notas];
+console.log(`A ${alunosnotas[0][1]} teve média igual a: ${alunosnotas[1][1]}`);
