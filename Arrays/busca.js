49678a07300f9d79616c36db7389241197369b86
Joao <joaovitor2004568@gmail.com>
2023-09-23 13:37:07 -0300

    usando os métodos .includes() e .indexOf() e desestruturando o código

--- Arrays/busca.js
+++ Arrays/busca.js
@@ -11,10 +11,11 @@ exibe(alunonome);
 function exibe(aluno){
 
     if(alunosnotas[0].includes(aluno)){
-
-        console.log(`${aluno} está cadastrado`);
-        const indice = alunosnotas[0].indexOf(aluno);
-        const media =  alunosnotas[1][indice];
+    
+        const [alunos,medias] = alunosnotas;
+        console.log(`${aluno} está na lista`);
+        const indice = alunos.indexOf(aluno);
+        const media = medias[indice];
         console.log(`A média de ${aluno} foi: ${media}`);
     }
     else {
