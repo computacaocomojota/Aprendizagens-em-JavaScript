05be575f15b4d68af38682d7a6538ec1799e7076
Joao <joaovitor2004568@gmail.com>
2023-10-18 07:43:53 -0300

    Padronizando string com o map()

--- Arrays/padroniza.js
+++ Arrays/padroniza.js
@@ -1,6 +1,4 @@
 const nomes = ['ANA luiza','YuRi','GaRRiDo','joao'];
 
-const nomesAtualizados = nomes.map((nome)=>{
-     return nome.toUpperCase();
-})
+const nomesAtualizados = nomes.map((nome)=> nome.toUpperCase());
 console.log(nomesAtualizados);
