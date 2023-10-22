52dbf45ac162e2f2966b1e832a42e65696d67529
Joao <joaovitor2004568@gmail.com>
2023-10-22 09:32:33 -0300

    Adicionando um Objeto em um outro Objeto

---
+++ Objetos/alinhado.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: 'João',
+    idade: 19,
+    telefone: ['75991377277','75992947299'],
+};
+cliente.endereco = {
+    rua: 'Alameda das Acácias',
+    bairro: 'Alto das Orquídeas',
+    apartamento: false,
+    numero: 14,
+    complemento: 'Casa',
+};
+
+console.log(cliente["endereco"])
