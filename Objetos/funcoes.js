28ced64c3602482fcfa1ecc588e4293069c24b82
Joao <joaovitor2004568@gmail.com>
2023-10-22 11:54:33 -0300

    Utilizando funções como propriedades de um objeto

---
+++ Objetos/funcoes.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome: 'João',
+    idade: 19,
+    email: 'joaovitor808569@gmail.com',
+    saldo: 200,
+    efetuaPagamento : function (valor) {
+    
+    if(valor>this.saldo) {
+    console.log("Saldo Insuficiente");
+    }
+    else{
+        
+        this.saldo -= valor;
+        console.log(`Pagamento realizado. Novo Saldo: ${this.saldo}`)
+    }
+}
+};
+
+cliente.efetuaPagamento(25);
