3e530c9de11b750b97f2a81a2e3a690cc52612fd
Joao <joaovitor2004568@gmail.com>
2024-01-02 21:52:06 -0300

    Aprendendo Insertion Sort

---
+++ Algoritmos/insertionSort.js
@@ -0,0 +1,22 @@
+const livros = require("./livros");
+
+function insertionSort(lista){
+
+    for(let atual = 0;atual<lista.length;atual++){
+        let analise = atual;
+            while(analise>0 && lista[analise].preco<lista[analise-1].preco){
+                
+                let [itemAnalise,itemAnterior] = [lista[analise],lista[analise-1]]
+
+                lista[analise] = itemAnterior;
+                lista[analise-1] = itemAnalise;
+
+                analise--;
+            }
+    }
+    return lista;
+}
+
+const livrosOrdenados = insertionSort(livros);
+console.log(livrosOrdenados);
+
