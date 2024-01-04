431eeda16565e9756256afe82ff865860005e3ef
Joao <joaovitor2004568@gmail.com>
2024-01-04 19:44:12 -0300

    Juntando e Ordenando listas

--- Algoritmos/juntaLista.js
+++ Algoritmos/juntaLista.js
@@ -3,7 +3,7 @@ const { edFolha, edGalho } = require("./arrays")
 function juntaLista(lista1, lista2) {
 
   let listaFinal = []
-  let [atualLista1, atualLista2, atualFinal] = [0, 0, 0]
+  let [atualLista1, atualLista2,] = [0,0]
 
   while (atualLista1 < lista1.length && atualLista2 < lista2.length) {
 
@@ -12,26 +12,23 @@ function juntaLista(lista1, lista2) {
 
     if (itemLista1.preco < itemLista2.preco) {
 
-      listaFinal[atualFinal] = itemLista1
+      listaFinal.push(itemLista1) 
       atualLista1++
     } else {
 
-      listaFinal[atualFinal] = itemLista2
+      listaFinal.push(itemLista2) 
       atualLista2++
     }
-    atualFinal++
   }
 
   while (atualLista1 < lista1.length) {
-    listaFinal[atualFinal] = lista1[atualLista1]
+    listaFinal.push(lista1[atualLista1])
     atualLista1++
-    atualFinal++
   }
 
   while (atualLista2 < lista2.length) {
-    listaFinal[atualFinal] = lista2[atualLista2]
+    listaFinal.push(lista2[atualLista2])  
     atualLista2++
-    atualFinal++
   }
   return listaFinal
 }
