96ac73426abce82eab337cbd23192e2cca6e55d1
Joao <joaovitor2004568@gmail.com>
2024-01-06 12:19:36 -0300

    Criando Rotas na API REST

--- API-REST/server.js
+++ API-REST/server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Curso de Node.js",
+  "/livros": "Entrei na Rota de Livros",
+  "/autores": "Entrei na Rota de Autores"
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 })
 
 server.listen(PORT, () => {
